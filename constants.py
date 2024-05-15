cc4bd17415be65f21a7ea17abf1cbc9e47a2b508
majd-ali <101995374+majd-ali@users.noreply.github.com>
2024-05-15 22:24:29 +0300

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,35 @@
+# ---------
+# CONSTANTS
+# ---------
+
+# --- PIXELS ---
+WIDTH = 600
+HEIGHT = 600
+#------MINE------
+width = 600
+height = 600
+
+ROWS = 3
+COLS = 3
+SQSIZE = width // COLS
+
+line_width = 15
+CIRC_WIDTH = 15
+CROSS_WIDTH = 20
+
+RADIUS = SQSIZE // 4
+
+OFFSET = 50
+
+# --- COLORS ---
+
+BG_COLOR = (0, 0, 0)
+line_color = (255, 255, 255)
+CIRC_COLOR = (0, 255, 0)
+CROSS_COLOR = (0, 0, 255)
+
+
+LINE_COLOR = (255, 255, 255)
+
+LINE_WIDTH = 15
+
